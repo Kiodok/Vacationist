e4ff2b883491a51d814c7f42eb71db7def356a06
Gary <Gary-Lude@web.de>
2026-05-31 14:34:38 +0200

    Updated to 1.3.0

--- apps/mobile/app.config.ts
+++ apps/mobile/app.config.ts
@@ -4,7 +4,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
   name: 'Vacationist',
   slug: 'vacationist',
-  version: '1.2.1',
+  version: '1.3.0',
   orientation: 'portrait',
   icon: './assets/images/icon.png',
   scheme: 'vacationist',
